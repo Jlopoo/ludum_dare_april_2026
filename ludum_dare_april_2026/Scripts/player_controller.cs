2085fe75470e88cdc7ce48513ae6975835a5b600
Jlopoo <jlopoo@cpromptgames.com>
2026-04-18 09:45:34 -0600

    player controller registers clicks and mouse position, added a background png

--- ludum_dare_april_2026/Scripts/player_controller.cs
+++ ludum_dare_april_2026/Scripts/player_controller.cs
@@ -3,24 +3,45 @@ using System;
 
 public partial class player_controller : Node2D
 {
-    public Vector2 mousePosition;
-    public Color mouseCircleColor;
+    public Vector2 lastMousePosition;
+    public Vector2 currentMousePosition;
+    public Color mouseCircleColor = new Color(1, 0, 0, 1);
 
     public override void _Ready()
     {
         base._Ready();
         GD.Print("PlayerController Node Ready");
+        lastMousePosition = GetGlobalMousePosition();
     }
 
     public override void _Process(double delta)
     {
         base._Process(delta);
-        mousePosition = GetGlobalMousePosition();
+        currentMousePosition = GetGlobalMousePosition();
+        if (lastMousePosition != currentMousePosition) {
+            lastMousePosition = currentMousePosition;
+            QueueRedraw();
+        }
     }
 
     public override void _Draw()
     {
-        base._Draw();
-        DrawCircle(mousePosition, 10, mouseCircleColor);
+        base._Draw(); 
+        DrawCircle(lastMousePosition, 10, mouseCircleColor);
+    }
+
+    public override void _UnhandledInput(InputEvent inputEvent)
+    {
+        base._UnhandledInput(inputEvent);
+        if (inputEvent is InputEventMouseButton mouseButtonEvent && mouseButtonEvent.ButtonIndex == MouseButton.Left)
+        {
+            mouseCircleColor = new Color(0, 1, 0, 1);
+            QueueRedraw();
+        }
+        else 
+        {
+            mouseCircleColor = new Color(1, 0, 0, 1);
+            QueueRedraw();
+        }
     }
 }
