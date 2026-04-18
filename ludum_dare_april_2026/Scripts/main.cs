e03130fc70ef23aa4030e969bceca8fb7c826c48
JGBM <mccord.john0@gmail.com>
2026-04-18 17:05:32 -0400

    moved options button to HUD instead of Main scene

--- ludum_dare_april_2026/Scripts/main.cs
+++ ludum_dare_april_2026/Scripts/main.cs
@@ -3,24 +3,10 @@ using Godot;
 public partial class main : Node2D
 {
 	private HUD hud;
-	private Button optionsButton;
 
 	public override void _Ready()
 	{
         base._Ready();
-
-		// Setup options gear button
-		optionsButton = GetNode<Button>("OptionsButton");
-		if (optionsButton != null)
-		{
-			optionsButton.Pressed += OnOptionsPressed;
-			GD.Print("Options button connected");
-		}
-
-		//Instance HUD
-		var hud_Scene = GD.Load<PackedScene>("res://Scenes/HUD.tscn");
-        var hud_Instance = hud_Scene.Instantiate<HUD>();
-        AddChild(hud_Instance);
 
 		StartGame();
 	}
@@ -29,17 +15,11 @@ public partial class main : Node2D
     {
 		GD.Print("StartGame() called");
 
-		// Instantiate HUD (includes Aurameter as a child)
 		PackedScene hudScene = GD.Load<PackedScene>("res://Scenes/HUD.tscn");
 		hud = hudScene.Instantiate<HUD>();
 		AddChild(hud);
 		GD.Print("HUD instantiated and added to scene");
     }
-
-	private void OnOptionsPressed()
-	{
-		GetTree().ChangeSceneToFile("res://Scenes/options_menu.tscn");
-	}
 
 	public override void _Process(double delta)
 	{
